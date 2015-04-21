756b413d222989237c8d76303f98789f2c523a9a
James Rossy <jm.rossy@gmail.com>
2015-04-20 21:55:11 -0700

    Added reason strings to end frames

--- js/quiz.js
+++ js/quiz.js
@@ -13,17 +13,19 @@ $(function() {
       , frame5 : [ "Right. Now how about the 'minting' process..."
                  , "Not quite. The coins are worth what others will pay for them." ]
 
-      , frameEndNo : [ ""
-                     , ""
-                     , "" 
-                     , "" 
-                     , ""
-                     , ""
-                     , "" 
-                     , "" 
-                     , "" ]
+      , frameEndNo : 
+            [ "The future of Bitcoin is still uncertain. It's a risky investment, not suitable for those without money they can afford to lose. You should build up some savings or invest in more traditional assets."
+             , "There's been a lot of money won off short-term specutaltion of the Bitcoin to fiat currency exchange rates. That ship has sailed. The easy money's been had. Investors looking for big short-term gains should look elsewhere."
+             , "Bitcoin is not backed by any fiat currencies. Neither is it based on a gold standard. Before you consider investing in Bitcoin, you should learn more about <a target=\"_blank\" href=\"http://www.investopedia.com/ask/answers/100314/why-do-bitcoins-have-value.asp\" title=\"Investopedia\">why it has value.</a>" 
+             , "Bitcoin is not based on a gold standard. Neither is it backed by any fiat currencies. Before you consider investing in Bitcoin, you should learn more about <a target=\"_blank\" href=\"http://www.investopedia.com/ask/answers/100314/why-do-bitcoins-have-value.asp\" title=\"Investopedia\">why it has value.</a>" 
+             , "Bitcoin is a decentralized system and there is no concept of 'Registration' or formal 'Sign up'. There's also no free cake for new users starting out. Before you consider investing in Bitcoin, you should learn more about <a target=\"_blank\" href=\"https://en.bitcoin.it/wiki/Mining\" title=\"Bitcoin Wiki\">how Bitcoins are created.</a>" 
+             , "Bitcoin is a decentralized system and no one entity, including the Bitcoin Foundation, controls coin generation. Before you consider investing in Bitcoin, you should learn more about <a target=\"_blank\" href=\"https://en.bitcoin.it/wiki/Mining\" title=\"Bitcoin Wiki\">how Bitcoins are created.</a>" 
+             , "Before you consider investing in Bitcoin, you should first learn about <a target=\"_blank\" href=\"http://en.wikipedia.org/wiki/The_birds_and_the_bees\" title=\"Bitcoin Wiki\">the birds and the bees</a> and then later about <a target=\"_blank\" href=\"https://en.bitcoin.it/wiki/Mining\" title=\"Bitcoin Wiki\">how Bitcoins are created.</a>" 
+             , "A 51% attack is when a single group (like a company or guild) controls more than half of the mining power on the Bitcoin network. If sustained, a 51% attack can compromise the integrity of the network and its history. Understanding about the balance of power is crucial in knowing why Bitcoin works. Before you consider investing in Bitcoin, you should learn more about <a target=\"_blank\" href=\"http://www.coindesk.com/51-attacks-real-threat-bitcoin/\" title=\"Coindesk article\">how power is distributed.</a>" 
+             , "Your concern is justified. There have been a few instances of guilds controlling <a target=\"_blank\" href=\"http://www.coindesk.com/51-attacks-real-threat-bitcoin/\" title=\"Coindesk article\">nearly half of the hash rate</a> on the network. If you're not confident the balance of power can be maintaned, then you shouldn't invest in Bitcoin." 
+             , "Bitcoin has the potential to enable scenarios like micropayments that existing payment processors can't easily support. But that doesn't mean incumbent players won't adapt. It's entirely possible that more 'traditional' methods for online transactions remain 'good enough'. It's entirely possible that Bitcoin remains a niche interest. If you don't believe that Bitcoin is necessary to enable new avenues for commerce, then you shouldn't invest." ]
 
-      , frameEndYes : ""
+      , frameEndYes : "You understand at least the basics of how Bitcoin works and seem to believe in its potential. If you haven't handled Bitcoins before, you should start small. Don't do anything crazy like sell your teeth to buy more coins. Learn about <a target=\"_blank\" href=\"https://bitcoin.org/en/secure-your-wallet\" title=\"Bitcoin Foundation\">keeping your wallet secure</a>. Be careful to buy only from reputable exchanges. And avoid the temptation to speculate on the exchange price. Happy coining %name%!"
     };
 
     var _UserName = "";
